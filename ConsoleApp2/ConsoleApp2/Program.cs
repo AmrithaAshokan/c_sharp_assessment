dc68e9067f750f098adaedda3d01b5c3bf93199a
Amritha Ashokan <114753462+AmrithaAshokan@users.noreply.github.com>
2022-11-12 15:15:06 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using ConsoleApp2;
+
+var mobile1 = new Mobile();
+mobile1.Set("User1", "A33 5G", 27000, "Samsung");
+
+var mobile2 = new Mobile();
+mobile2.Set("User2", "Nord", 20000);
+
+mobile1.Get();
+mobile2.Get();
---
+++ ConsoleApp2/ConsoleApp2/mobile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Mobile
+    {
+        public string? Name;
+        public string? Model;
+        public int Price;
+        public string? Company;
+
+
+        public void Set(string name, string model, int price, string company)
+        { 
+            Name = name;
+            Model = model;
+            Price = price; 
+            Company = company; 
+        }
+
+        public void Set(string name, string model, int price)
+        {
+            Name = name;
+            Model = model;
+            Price = price;
+            Company = "One Plus";
+        }
+        public void Get()
+        {
+            Console.WriteLine($"\nName: {Name}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Company: {Company}");
+            Console.WriteLine($"Price: {Price}\n");
+
+        }
+
+    };
+}
+
