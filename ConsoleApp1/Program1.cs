6e1f665307936852a5ecd5f7a21de4c7b8bbfb48
Amritha Ashokan <114753462+AmrithaAshokan@users.noreply.github.com>
2022-11-12 15:12:30 +0530

    Add files via upload

---
+++ ConsoleApp1/Program1.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp1;
+
+var employee1 = new Employee()
+{
+    Name = "Employee1",
+    Department = "HR",
+    Designation = "Trainee"
+};
+var employee2 = new Employee()
+{
+    Name = "Employee2",
+    Department = "ESS",
+    Designation = "Manager"
+};
+var employee3 = new Employee()
+{
+    Name = "Employee3",
+    Department = "PSS",
+    Designation = "Senior Employee"
+};
+employee1.ShowData();
+employee2.ShowData();
+employee3.ShowData();
+
+
+
---
+++ ConsoleApp1/employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+internal class Employee
+{
+    public string Name { get; set; }
+    public string Department { get; set; }
+    private string _designation { get; set; }
+    private int  _salary;
+
+
+    public int Salary
+    {
+        get 
+        {
+            return _salary; 
+        }
+        set 
+        {
+            _salary= value;
+        }
+    }
+
+    public string Designation
+    {
+        get 
+        {
+            return _designation; 
+        }
+        set
+        {
+            if( value == "Trainee")
+            {
+                Salary = 5000;
+            }
+            else if (value == "Manager")
+            {
+                Salary = 100000;
+            }
+            else if (value == "Senior Employee")
+            {
+                Salary = 75000;
+            }
+            _designation = value;
+        }
+   
+   
+    }
+    public void ShowData()
+    {
+        Console.WriteLine($"\nName : {Name} ");
+        Console.WriteLine($"Department : {Department}");
+        Console.WriteLine($"Designation : {Designation}");
+        Console.WriteLine($"Salary: {Salary}\n");
+        
+
+    }
+
+
+}
