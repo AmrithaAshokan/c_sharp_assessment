4e17785da5f8b692262e347d13d3fbd2846db0cb
Amritha Ashokan <114753462+AmrithaAshokan@users.noreply.github.com>
2022-11-12 17:27:56 +0530

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using ConsoleApp4;
+
+var food1 = new food();
+Console.WriteLine("Food Name: ");
+food1.FoodName = Console.ReadLine();
+Console.WriteLine("Price: ");
+food1.Price = int.Parse(Console.ReadLine());
+food1.ShowData();
+
---
+++ ConsoleApp4/ConsoleApp4/food.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp4
+{
+    internal class food
+    {
+        public string FoodName;
+        private double _price;
+
+        private double _discount;  
+
+        public double Price 
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value > 150)
+                {
+                    value = (value - (0.1 * value));
+                }
+                _price = value;
+            }
+            
+        }
+        public void ShowData()
+        {
+            Console.WriteLine($"\nName : {FoodName}");
+            Console.WriteLine($"Price : {Price}");
+
+        }
+
+
+    }
+    
+
+}
